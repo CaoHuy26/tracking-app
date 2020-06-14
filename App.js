b0f776a23296f7d439592a4f1b6e6087444f714f
CaoHuy26 <huyc197@gmail.com>
2020-06-14 16:39:47 +0700

    Split Maps component

--- App.js
+++ App.js
@@ -1,63 +1,20 @@
-import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
-
-// const initialState  = {
-//   latitude: 21.0245,
-//   longitude: 105.84117,
-//   latitudeDelta: 0.09,
-//   longitudeDelta: 0.035
-// }
-
-const initialState = {
-  latitude: null,
-  longitude: null,
-  latitudeDelta: 0.0922,
-  longitudeDelta: 0.0421,
-};
+import React from "react";
+import { StyleSheet, StatusBar, View } from "react-native";
+import Maps from './src/components/Maps';
 
 const App = () => {
-  const [currentPosition, setCurrentPosition] = useState(initialState);
-
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        // alert(JSON.stringify(position));
-        const { latitude, longitude } = position.coords;
-        setCurrentPosition({
-          ...currentPosition,
-          latitude,
-          longitude
-        })
-      },
-      (error) => alert(error.message),
-      { timeout: 20000, maximumAge: 1000 }
-    );
-  }, []);
-
-  return currentPosition.latitude ? (
-    // 21.0245, 105.84117: Hanoi
-    <MapView
-      style={styles.map}
-      provider={PROVIDER_GOOGLE}
-      showsUserLocation
-      region={currentPosition}
-    ></MapView>
-  ) : (
-    <ActivityIndicator style={{ flex: 1 }} animating size="large" />
-  );
-};
+  return (
+    <View style={styles.container}>
+      <StatusBar hidden/>
+      <Maps />
+    </View>
+  )
+}
 
 export default App;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  map: {
-    flex: 1,
-  },
-});
+    flex: 1
+  }
+})
---
+++ src/components/Maps.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import { ActivityIndicator } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+
+// 21.0245, 105.84117: Hanoi
+const initialState = {
+  latitude: null,
+  longitude: null,
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421,
+};
+
+const Maps = () => {
+  const [currentPosition, setCurrentPosition] = useState(initialState);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // alert(JSON.stringify(position));
+        const { latitude, longitude } = position.coords;
+        setCurrentPosition({
+          ...currentPosition,
+          latitude,
+          longitude
+        })
+      },
+      (error) => alert(error.message),
+      { timeout: 20000, maximumAge: 1000 }
+    );
+  }, []);
+
+  return currentPosition.latitude ? (
+    <MapView
+      style={{ flex: 1 }}
+      provider={PROVIDER_GOOGLE}
+      showsUserLocation
+      region={currentPosition}
+    ></MapView>
+  ) : (
+    <ActivityIndicator style={{ flex: 1 }} animating size="large" />
+  );
+};
+
+export default Maps;
